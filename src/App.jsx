9b52a95222dd5602cba3ac443f853f8fc0e95099
SamsShow <sakshamtyagi2008@gmail.com>
2024-11-09 17:12:31 +0530

    ⚡️ ✨ Add UserDashboard component and integrate axios for API calls

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage';
 import AdminPage from './components/AdminPage';
 import HospitalDashboard from './components/HospitalDashboard';
 import CompanyDashboard from './components/CompanyDasboard';
+import UserDashboard from './components/UserDashboard';
 
 const App = () => (
     <Router>
@@ -11,6 +12,7 @@ const App = () => (
             <Route path="/admin" element={<AdminPage />} />
             <Route path='/hospital' element={<HospitalDashboard />} />
             <Route path='/company' element={<CompanyDashboard />} />
+            <Route path='/user' element={<UserDashboard />} /> 
         </Routes>
     </Router>
 );
